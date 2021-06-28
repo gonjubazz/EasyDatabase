05b68a3ebfed26cfd545babad58538f6a899143f
gonjubazz <gonjubazz@gmail.com>
2021-06-28 11:15:23 +0500

    update

--- Telegram/telegram_db_script.py
+++ Telegram/telegram_db_script.py
@@ -1,18 +1,25 @@
 import telebot
 from TxtDatabase import DataBase
 
-database = DataBase("telegram_db.txt")
+users_database = DataBase("telegram_users_db.txt")
+messages_database = DataBase("telegram_messages_db.txt")
+
 bot = telebot.TeleBot("1824586460:AAGZ1WMqvKgInBdUgjaRFAiB70aX265X7zU")
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(content_types=["text"])
 def add_to_database(message):
-    if database.get_variable(f'{message.from_user.first_name} {message.from_user.last_name}') is None:
-        database.new_variable(f'{message.from_user.first_name} {message.from_user.last_name}', "int",
-                              message.from_user.id)
-        print("this user are new")
+    if users_database.get_variable(f'{message.from_user.first_name} {message.from_user.last_name}') is None:
+        users_database.new_variable(f'{message.from_user.first_name} {message.from_user.last_name}', "int",
+                                    message.from_user.id)
+        messages_database.new_variable(f'{message.from_user.first_name} {message.from_user.last_name}', "list",
+                                       [message.text])
+        print("New user!")
     else:
-        print("this user is in database")
+        messages_database.set_variable(f'{message.from_user.first_name} {message.from_user.last_name}', "list",
+                                       messages_database.get_variable(
+                                           f'{message.from_user.first_name} {message.from_user.last_name}').value + [
+                                           message.text])
 
 
 bot.polling()
